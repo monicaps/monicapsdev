b1206ae4611350e9b9f0fd0fea6f64e617401864
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-09-30 01:57:38 -0500

    base de maquetado de sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { CommunityComponent } from './community/community.component';
+import { InitiativesComponent } from './initiatives/initiatives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ContactComponent,
+    CommunityComponent,
+    InitiativesComponent
   ],
   imports: [
     BrowserModule,
