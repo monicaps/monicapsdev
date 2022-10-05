ed9a2df359444105369b53ccff87f08bfdd2e238
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2022-10-05 02:35:37 -0500

    estructtura base de sección comunidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { CommunityComponent } from './community/community.component';
+import { InitiativesComponent } from './initiatives/initiatives.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact',
+    component:ContactComponent
+  },
+  {
+    path: 'projects',
+    component:ProjectsComponent
+  },
+  {
+    path: 'initiatives',
+    component:InitiativesComponent
+  },
+  {
+    path: 'community',
+    component:CommunityComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
